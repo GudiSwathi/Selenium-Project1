957d736992e213fba3e13f8fc8edc12e002f024c
GudiSwathi <swathi.gudi24@gmail.com>
2020-04-02 17:18:38 +0530

    Commented Mercury Registration Test

--- MercuryTours/Tests/SampleDataDrivenTesting.cs
+++ MercuryTours/Tests/SampleDataDrivenTesting.cs
@@ -31,22 +31,22 @@ namespace MercuryTours.Tests
             MercuryToursHomePage homeLink = mercuryRegistrationPage.NavigateToHomePage();
         }
 
-        public void MercuryToursRegistrationTest2()
-        {
-            List<RegistrationInformation> registrationInformation = GetTestData.GetRegistrationInformation();
-            RegistrationInformation requiredRegistrationInfo = registrationInformation.
-                Where(x => x.ContactInformation.FirstName.Equals("Buddi", StringComparison.InvariantCultureIgnoreCase)).Select(x => x).FirstOrDefault();
+        //public void MercuryToursRegistrationTest2()
+        //{
+        //    List<RegistrationInformation> registrationInformation = GetTestData.GetRegistrationInformation();
+        //    RegistrationInformation requiredRegistrationInfo = registrationInformation.
+        //        Where(x => x.ContactInformation.FirstName.Equals("Buddi", StringComparison.InvariantCultureIgnoreCase)).Select(x => x).FirstOrDefault();
 
-            MercuryRegistrationPage mercuryRegistrationPage = new MercuryRegistrationPage(SeleniumActions);
-            bool isFooterAvailable = mercuryRegistrationPage.CheckIfFooterTextIsDisplayed("© 2005, Mercury Interactive (v. 011003-1.01-058)");
-            Assert.IsTrue(isFooterAvailable, "Footer text is not available");
+        //    MercuryRegistrationPage mercuryRegistrationPage = new MercuryRegistrationPage(SeleniumActions);
+        //    bool isFooterAvailable = mercuryRegistrationPage.CheckIfFooterTextIsDisplayed("© 2005, Mercury Interactive (v. 011003-1.01-058)");
+        //    Assert.IsTrue(isFooterAvailable, "Footer text is not available");
 
-            mercuryRegistrationPage.RegisterTheFirstUser(requiredRegistrationInfo);
+        //    mercuryRegistrationPage.RegisterTheFirstUser(requiredRegistrationInfo);
 
-            //bool isRegistrationTextAvailable = mercuryRegistrationPage.CheckIfRegistrationTextIsDisplayed("Thank you for registering. You may now sign-in using the user name and password you've just entered.");
-            //Assert.IsTrue(isRegistrationTextAvailable, "Registration text is not available");
-            MercuryToursHomePage homeLink = mercuryRegistrationPage.NavigateToHomePage();
-        }
+        //    //bool isRegistrationTextAvailable = mercuryRegistrationPage.CheckIfRegistrationTextIsDisplayed("Thank you for registering. You may now sign-in using the user name and password you've just entered.");
+        //    //Assert.IsTrue(isRegistrationTextAvailable, "Registration text is not available");
+        //    MercuryToursHomePage homeLink = mercuryRegistrationPage.NavigateToHomePage();
+        //}
 
         [Test]
         public void MercuryHomePageTest()
